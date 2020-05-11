673876d968f9b5f03b833b463ca7f97c924458af
Nick Orsini <61241003+Norsini598@users.noreply.github.com>
2020-05-11 16:39:22 -0400

    Delete Instantiation.cpp

--- Instantiation.cpp
+++
@@ -1,9 +0,0 @@
-#include "Main.cpp"
-#include "BinaryTree.cpp"
-#include "TreeNode.cpp"
-
-template class TreeNode<int>;
-template class TreeNode<long>;
-
-template class BinaryTree<int>;
-template class BinaryTree<long>;
